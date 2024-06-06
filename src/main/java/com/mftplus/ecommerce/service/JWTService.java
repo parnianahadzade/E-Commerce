300bbe0267450e9b725092b23b020b3872ad0d0c
kurromii <ahadzadeparnian@gmail.com>
2024-06-06 08:31:20 +0330

    jwt token security bug fixed added

--- src/main/java/com/mftplus/ecommerce/service/JWTService.java
+++ src/main/java/com/mftplus/ecommerce/service/JWTService.java
@@ -2,6 +2,7 @@ package com.mftplus.ecommerce.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.mftplus.ecommerce.model.entity.User;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -49,8 +50,11 @@ public class JWTService {
     }
 
     public String getUsername(String token){
+
+        DecodedJWT jwt = JWT.require(algorithm).withIssuer(issuer).build().verify(token);
+
         //getting the username from token
         // .decode throws jwtDecodeException
-        return JWT.decode(token).getClaim(USERNAME_KEY).asString();
+        return jwt.getClaim(USERNAME_KEY).asString();
     }
 }
--- src/test/java/com/mftplus/ecommerce/service/JWTServiceTest.java
+++ src/test/java/com/mftplus/ecommerce/service/JWTServiceTest.java
@@ -1,10 +1,15 @@
 package com.mftplus.ecommerce.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.MissingClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.mftplus.ecommerce.model.entity.User;
 import com.mftplus.ecommerce.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,8 +19,12 @@ public class JWTServiceTest {
 
     @Autowired
     private JWTService jwtService;
+
     @Autowired
     private UserRepository userRepository;
+
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
 
     @Test
     public void testVerificationTokenNotUsableForLogin(){
@@ -36,5 +45,25 @@ public class JWTServiceTest {
                 jwtService.getUsername(token),
                         " token for auth should contain users username.");
 
+    }
+
+    @Test
+    public void testJwtNotGeneratedByUs(){
+        String token =
+        JWT.create().withClaim("USERNAME", "UserA").sign(Algorithm.HMAC256(
+                "NotTheRealSecret"));
+
+        Assertions.assertThrows(SignatureVerificationException.class,
+                () -> jwtService.getUsername(token));
+
+    }
+
+    @Test
+    public void testJWTCorrectlySignedNoIssuer(){
+        String token =
+                JWT.create().withClaim("USERNAME", "UserA").sign(Algorithm.HMAC256(
+                        algorithmKey));
+        Assertions.assertThrows(MissingClaimException.class,
+                () -> jwtService.getUsername(token));
     }
 }
